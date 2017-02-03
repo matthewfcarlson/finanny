67ddb5f61995dcbbcf91273e1f0eb9021ca04a67
Matthew Carlson <matthewfcarlson@gmail.com>
2017-02-02 17:09:03 -0800

    Let's try again

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/css'));
 
 
-app.all('*', function(req, res, next) {
+app.all('/*', function(req, res, next) {
   // Just send the index.html for other files to support HTML5Mode
   res.sendFile('/public/index.html', { root: __dirname });
 });
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
