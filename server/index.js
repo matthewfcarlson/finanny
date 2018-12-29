8dd0a11d3ce1384d9b089f02b4d314d7ee26248b
Matthew Carlson <macarl@microsoft.com>
2018-12-29 00:41:55 -0600

    upgraded to vue,js

---
+++ server/index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+const path = require("path");
+var http = require("http").Server(app);
+const history = require("connect-history-api-fallback");
+
+app.use("/", express.static(path.join(__dirname, "../dist")));
+
+app.get("/", function (req, res) {
+    //res.send("<h1>Hello World</h1>");
+    res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
+app.use(
+    history({
+        disableDotRule: true,
+        verbose: true
+    })
+);
+
+app.get("/index.html", function (req, res) {
+    //res.send("<h1>Hello World</h1>");
+    res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
+
+const port = process.env.PORT || 3000;
+http.listen(port, function () {
+    console.log("Listening on " + port);
+});
+
