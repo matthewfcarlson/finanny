d43b900350c1eb06fd7855db5cab076ba20a65b1
Matthew Carlson <macarl@microsoft.com>
2018-12-29 10:59:04 -0600

    using feathers.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
-var express = require("express");
-var app = express();
 const path = require("path");
-var http = require("http").Server(app);
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+const socketio = require('@feathersjs/socketio');
+
 const history = require("connect-history-api-fallback");
+
+var app = express(feathers());
+
+// Parse HTTP JSON bodies
+app.use(express.json());
+// Parse URL-encoded params
+app.use(express.urlencoded({
+    extended: true
+}));
+// Add REST API support
+app.configure(express.rest());
+// Configure Socket.io real-time APIs
+app.configure(socketio());
+
 
 app.use(express.static(path.join(__dirname, "../dist")));
 app.use(express.static(path.join(__dirname, "../dist/css")));
@@ -24,9 +39,18 @@ app.get("/index.html", function (req, res) {
     res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
+// Register a nicer error handler than the default Express one
+app.use(express.errorHandler());
+
+
+//SOCKET.IO stuff
+// Add any new real-time connection to the `everybody` channel
+app.on('connection', connection => app.channel('everybody').join(connection));
+// Publish all events to the `everybody` channel
+app.publish(data => app.channel('everybody'));
+
 
 const port = process.env.PORT || 3000;
-http.listen(port, function () {
+app.listen(port).on('listening', function () {
     console.log("Listening on " + port);
 });
-
